52cb4c81fc0f6dc7e006baafb76d84f7c28540bf
ecuadros <ecuadros@spc.org.pe>
2024-07-21 11:48:32 -0500

    02-class

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
+#include <iostream>  // cout
+#include "student.h" // CStudent
 
 // Github: https://github.com/ecuadros/test.git
 // g++ -pthread -std=c++17 -o main main.cpp 
 
-// Buscar por defatult en std.
+// Buscar por default en std.
 // Esto sirve para simplificar std::cout a solamente cout
 using namespace std;
 
 int main(){
       cout << "Test 2" << endl;
+      CStudent gourab, er; // stud1 es un objeto
+
       return 0;
 }
---
+++ student.h
@@ -0,0 +1,9 @@
+#ifndef __STUDENT_H__
+#define __STUDENT_H__
+
+// Definición de una clase
+class CStudent{
+
+};
+
+#endif
