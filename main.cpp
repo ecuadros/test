fee01e93609ea4f34658ba3dfa12b8e83c6a93ae
ecuadros <ecuadros@spc.org.pe>
2024-07-21 11:35:17 -0500

    01-Main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-void main()
-{
-      cout << "Hello world" << endl;
-      cout << "Test" << endl;
+#include <iostream>
+
+// Github: https://github.com/ecuadros/test.git
+// g++ -pthread -std=c++17 -o main main.cpp 
+
+// Buscar por defatult en std.
+// Esto sirve para simplificar std::cout a solamente cout
+using namespace std;
+
+int main(){
+      cout << "Test 2" << endl;
+      return 0;
 }
